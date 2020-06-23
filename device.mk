8c6a22acd9673a94212e59455cd46c2e399e6daa
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-06-23 18:32:11 +0530

    laurel_sprout: Up-rev Health hal from 2.0 to 2.1.
    Change-Id: Ib2d1e7742b49919045022d385460e0a3fedf3033

--- device.mk
+++ device.mk
@@ -240,7 +240,9 @@ LOC_HIDL_VERSION = 4.0
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
