379f66159c62a9355ccc3a85f2ae3c7081cf4d0b
Murthy Nidadavolu <mnssr@codeaurora.org>
2020-06-26 11:21:56 +0530

    laurel_sprout: Remove older drm HAL entries
    Remove older DRM HAL entries to comply hw compatibility matrix.
    
    Change-Id: Iec373241d0bb1c39002398cb149235e74497f2c2

--- device.mk
+++ device.mk
@@ -212,8 +212,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
 # FM
