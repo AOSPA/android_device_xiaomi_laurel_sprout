04d17d26903114cf594cd0b636d1a4cb7772b706
Vishalcj17 <vishalcj@aospa.co>
2020-07-25 14:33:30 +0530

    laurel_sprout: Import light hal from ginkgo
    Adapted for laurel_sprout.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Ic85d502a3d67b67f93a2516a5e38cce733dacc82

--- device.mk
+++ device.mk
@@ -301,6 +301,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.laurel_sprout
+
 # Sensor HAL conf file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
