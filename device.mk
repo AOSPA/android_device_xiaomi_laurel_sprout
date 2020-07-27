d8bbdb84f5c4c4a9f5df32f2f6d7a7a361ca1242
Vishalcj17 <vishalcj@aospa.co>
2020-07-27 22:42:13 +0530

    laurel_sprout: build thermal hidl from source
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I619af3d60a2de0df9b9bd2e6b9de4cfad243181d

--- device.mk
+++ device.mk
@@ -408,6 +408,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
