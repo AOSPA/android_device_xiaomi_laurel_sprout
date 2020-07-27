5c5e9d6216f3a0396e0997973c9bba72b11af0e2
Vishalcj17 <vishalcj@aospa.co>
2020-07-27 16:33:56 +0530

    laurel_sprout: Import ramdisk from CAF
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I6f96b2442d11de6347daf019e0a023fe1fe36802

--- device.mk
+++ device.mk
@@ -266,6 +266,19 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.msm.usb.configfs.rc \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.rc \
+    init.recovery.qcom.rc \
+    init.qcom.sensors.sh \
+    init.qti.chg_policy.sh \
+    init.qcom.sh \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    init.target.rc \
+    ueventd.qcom.rc \
     fstab.qcom
 
 # RCS
