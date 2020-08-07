cde2aa6697cf614f1e51c7688c87d721d460e97b
Vishalcj17 <vishalcj@aospa.co>
2020-08-07 09:28:22 +0530

    laurel_sprout: Kang sepolicy from cepheus
    *inserts troll_face*
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I9a84273b33db7b41874cb609d8eb3f326000b50b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,6 +171,11 @@ TARGET_NO_RECOVERY := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # RenderScript
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
