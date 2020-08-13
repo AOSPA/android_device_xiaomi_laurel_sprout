d868be15e0ca0906f8a238f4ed803d45da5ff63f
Vishalcj17 <vishalcj@aospa.co>
2020-08-13 09:14:55 +0530

    laurel_sprout: import thermal from stock
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I7162e6ea07c16c837a872205c0a62ee32297bb11

--- device.mk
+++ device.mk
@@ -422,6 +422,14 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-high.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-high.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-nolimits.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-nolimits.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
