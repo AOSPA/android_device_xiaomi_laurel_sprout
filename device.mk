201e61f302246ec8a9240b06554e12dc5f938491
Kavya Nunna <knunna@codeaurora.org>
2020-08-17 15:55:28 +0530

    laurel_sprout: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.
    Also inherit Vibrator AIDL
    Change-Id: I2286c5920126dde1dabd66c2ba8ca5c3315ff331
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- device.mk
+++ device.mk
@@ -356,9 +356,7 @@ PRODUCT_PACKAGES += \
 VENDOR_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # Ramdisk
 PRODUCT_PACKAGES += \
