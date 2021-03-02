6d950561cb8e281ca0bb9c8a1a81ffd5926e48a4
Vishalcj17 <vishalcj@aospa.co>
2021-03-02 19:43:32 +0530

    laurel: Update FOD dimming formula.
    This is based on simple parabolic weights formula calculations.
    
    Thanks for @markakash and @dev-harsh1998 for helping me out.
    
    Change-Id: I66650373d3508c5673d74d9481d06e941bc20cc9

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -29,8 +29,8 @@
 #define PARAM_NIT_NONE 0
 
 #define DISPPARAM_PATH "/sys/devices/platform/soc/5e00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/disp_param"
-#define DISPPARAM_FOD_BACKLIGHT_HBM "0x1d007ff"
-#define DISPPARAM_FOD_BACKLIGHT_RESET "0x20f0000"
+#define DISPPARAM_FOD_BACKLIGHT_HBM "0x1D20000"
+#define DISPPARAM_FOD_BACKLIGHT_RESET "0x20F0000"
 
 #define FOD_STATUS_PATH "/sys/class/touch/tp_dev/fod_status"
 #define FOD_STATUS_ON 1
@@ -117,15 +117,9 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
 }
 
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
-    float alpha;
+    double dim = (255 + ((-12.08071) * pow((double)brightness, 0.355)));
 
-    if (brightness > 62.0) {
-        alpha = 1.0 - pow((((brightness / 255.0) * 430.0) / 600.0), 0.455);
-    } else {
-        alpha = 1.0 - pow((brightness / 210.0), 0.455);
-    }
-
-    return 255 * alpha;
+    return (int32_t)dim;
 }
 
 Return<bool> FingerprintInscreen::shouldBoostBrightness() {
