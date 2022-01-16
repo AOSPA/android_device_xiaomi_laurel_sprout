592e30829708d7654aa891fbe98deb6a91d037e4
Vishalcj17 <vishalcj@aospa.co>
2022-01-16 14:06:08 +0530

    laurel: Switch to UDFPS.
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Change-Id: I7c7ac70536b76c29e936798fad6f50acbfb9a68c

--- device.mk
+++ device.mk
@@ -56,11 +56,10 @@ PRODUCT_COPY_FILES += \
 
 # Biometrics
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-    vendor/pa/config/permissions/vendor.aospa.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/vendor.aospa.biometrics.fingerprint.inscreen.xml
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    vendor.aospa.biometrics.fingerprint.inscreen@1.0-service
+    android.hardware.biometrics.fingerprint@2.3-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
